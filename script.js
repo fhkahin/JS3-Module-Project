89f074b2f4e0737a907b69acc91286aae65f67b3
Fathi Kahin <fhkahin@gmail.com>
2024-01-26 13:11:33 +0000

    level 100 script completed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +5,37 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(episode => {
+      const episodeContainer = document.createElement("div");
+      episodeContainer.classList.add("episode");
+
+      // Here I created the episode code
+      const episodeCode = `S${pad(episode.season)}E${pad(episode.number)}`;
+
+      // Then I created  the episode title
+      const titleElem = document.createElement("h2");
+      titleElem.textContent = `${episode.name} - ${episodeCode}`;
+      episodeContainer.appendChild(titleElem);
+
+      // Then I createa the  episode image
+      const imageElem = document.createElement("img");
+      imageElem.src = episode.image.medium;
+      imageElem.alt = `${episode.name} Image`;
+      episodeContainer.appendChild(imageElem);
+
+      // after the I  created the  episode summary
+      const summaryElem = document.createElement("div");
+      summaryElem.innerHTML = episode.summary;
+      episodeContainer.appendChild(summaryElem);
+
+      rootElem.appendChild(episodeContainer);
+  });
+
+  // Pad a number with leading zeros to make it two digits
+  function pad(number) {
+      return String(number).padStart(2, '0');
+  }
 }
 
 window.onload = setup;
