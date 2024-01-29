3c10b02ba6065524cc500284d4ac72745540eb66
Pedro <riccip@outlook.com>
2024-01-29 09:55:13 +0000

    created a component for the episode card

--- script.js
+++ script.js
@@ -7,35 +7,42 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
   episodeList.forEach(episode => {
-      const episodeContainer = document.createElement("div");
-      episodeContainer.classList.add("episode");
-
-      // Here I created the episode code
-      const episodeCode = `S${pad(episode.season)}E${pad(episode.number)}`;
-
-      // Then I created  the episode title
-      const titleElem = document.createElement("h2");
-      titleElem.textContent = `${episode.name} - ${episodeCode}`;
-      episodeContainer.appendChild(titleElem);
-
-      // Then I createa the  episode image
-      const imageElem = document.createElement("img");
-      imageElem.src = episode.image.medium;
-      imageElem.alt = `${episode.name} Image`;
-      episodeContainer.appendChild(imageElem);
-
-      // after the I  created the  episode summary
-      const summaryElem = document.createElement("div");
-      summaryElem.innerHTML = episode.summary;
-      episodeContainer.appendChild(summaryElem);
-
-      rootElem.appendChild(episodeContainer);
+    const episodeCard = makeEpisodeCard(episode);
+    rootElem.appendChild(episodeCard);
   });
+}
+
+
+// Splitted the logic to create the episode card (Actually, I prefer to make a new file for this component)
+function makeEpisodeCard(episode) {
+  const episodeContainer = document.createElement("div");
+  episodeContainer.classList.add("episode");
+
+  // Here I created the episode code
+  const episodeCode = `S${pad(episode.season)}E${pad(episode.number)}`;
+
+  // Then I created  the episode title
+  const titleElem = document.createElement("h2");
+  titleElem.textContent = `${episode.name} - ${episodeCode}`;
+  episodeContainer.appendChild(titleElem);
+
+  // Then I create the  episode image
+  const imageElem = document.createElement("img");
+  imageElem.src = episode.image.medium;
+  imageElem.alt = `${episode.name} Image`;
+  episodeContainer.appendChild(imageElem);
+
+  // after that I  created the  episode summary
+  const summaryElem = document.createElement("div");
+  summaryElem.innerHTML = episode.summary;
+  episodeContainer.appendChild(summaryElem);
 
   // Pad a number with leading zeros to make it two digits
   function pad(number) {
-      return String(number).padStart(2, '0');
+    return String(number).padStart(2, '0');
   }
+
+  return episodeContainer;
 }
 
 window.onload = setup;
